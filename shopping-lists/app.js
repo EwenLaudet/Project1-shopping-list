b1ccdfbb22516504630d321b50c2d0aa5f899cf9
ewen <laudet.ewen35@gmail.com>
2023-07-03 12:04:46 +0200

    collect items and associated tests

--- shopping-lists/app.js
+++ shopping-lists/app.js
@@ -2,6 +2,7 @@ import { serve } from "./deps.js";
 import { configure } from "./deps.js";
 import * as listController from "./controllers/listController.js";
 import * as shoppingListItemController from "./controllers/shoppingListItemController.js";
+import * as requestUtils from "./utils/requestUtils.js";
 
 
 configure({
@@ -12,18 +13,15 @@ const handleRequest = async (request) => {
   const url = new URL(request.url);
 
   if (url.pathname === "/" && request.method === "GET") {
-    return new Response(`Redirecting to /lists.`, {
-      status: 303,
-      headers: {
-        "Location": "/lists",
-      },
-    });
+    return requestUtils.redirectTo("/lists");
   } else if (url.pathname === "/lists" && request.method === "POST") {
     return await listController.addList(request);
   } else if (url.pathname === "/lists" && request.method === "GET") {
     return await listController.viewLists(request);
   } else if (url.pathname.match("lists/[0-9]+") && request.method === "GET") {
     return await listController.viewList(request);
+  } else if (url.pathname.match("lists/[0-9]+/items/[0-9]+/collect") && request.method === "POST") {
+    return await shoppingListItemController.collectShoppingListItem(request);
   } else if (url.pathname.match("lists/[0-9]+/items") && request.method === "POST") {
     return await shoppingListItemController.createShoppingListItem(request);
   } else {
